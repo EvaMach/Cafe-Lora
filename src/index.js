e90b8bd43a3364079046b7ee154c765c98a3ca9b
Eva Machova <evca.machova@gmail.com>
2022-05-06 19:40:18 +0200

    fix navigation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css';
 
-console.log('funguju!');
+// 3 ZPROVOZNĚNÍ NAVIGACE
+const navigation = document.querySelector('nav');
+
+document.querySelector('#nav-btn').addEventListener('click', () => {
+  navigation.classList.toggle('nav-closed');
+});
+
+document.querySelectorAll('nav > a').forEach((a) => {
+  a.addEventListener('click', () => {
+    navigation.classList.add('nav-closed');
+  });
+});

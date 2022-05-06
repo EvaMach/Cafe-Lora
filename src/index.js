36c93cbd1310a2ad75bf0cf0aff51fa741eb687e
Eva Machova <evca.machova@gmail.com>
2022-05-06 20:14:42 +0200

    order behaviour

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './style.css';
 
 // 3 ZPROVOZNĚNÍ NAVIGACE
 const navigation = document.querySelector('nav');
-
 document.querySelector('#nav-btn').addEventListener('click', () => {
   navigation.classList.toggle('nav-closed');
 });
@@ -11,4 +10,20 @@ document.querySelectorAll('nav > a').forEach((a) => {
   a.addEventListener('click', () => {
     navigation.classList.add('nav-closed');
   });
+});
+
+// 4 OBJEDNÁVÁNÍ
+const orderBtn = document.querySelector('.order-btn');
+let ordered = false;
+
+orderBtn.addEventListener('click', () => {
+  ordered = ordered === false ? true : false;
+  if (ordered) {
+    orderBtn.textContent = 'Zrušit';
+  } else {
+    orderBtn.textContent = 'Objednat';
+  }
+  document
+    .querySelector('.drink__cup')
+    .classList.toggle('drink__cup--selected');
 });

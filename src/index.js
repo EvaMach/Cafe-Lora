5c66ca8f2a5f7bb458ed9321b3db25ea95b0638f
Eva Machova <evca.machova@gmail.com>
2022-05-06 21:49:13 +0200

    add api and polish

--- src/index.js
+++ src/index.js
@@ -13,66 +13,10 @@ document.querySelectorAll('nav > a').forEach((a) => {
   });
 });
 
-// 5 INGREDIENCE JAKO KOMPONENTY
-
-// 2.2 NÁPOJ JAKO KOMPONENTA
-
-// const drinkData = {
-//   id: 'romano',
-//   name: 'Romano',
-//   ordered: false,
-//   image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
-//   layers: [
-//     {
-//       color: '#fbdf5b',
-//       label: 'citrón',
-//     },
-//     {
-//       color: '#613916',
-//       label: 'espresso',
-//     },
-//   ],
-// };
-
-const drinks = [
-  {
-    id: 'cappuccino',
-    name: 'Cappuccino',
-    ordered: false,
-    layers: [
-      {
-        color: '#feeeca',
-        label: 'mléčná pěna',
-      },
-      {
-        color: '#fed7b0',
-        label: 'teplé mléko',
-      },
-      {
-        color: '#613916',
-        label: 'espresso',
-      },
-    ],
-    image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/cappuccino.png',
-  },
-  {
-    id: 'romano',
-    name: 'Romano',
-    ordered: false,
-    layers: [
-      {
-        color: '#fbdf5b',
-        label: 'citrón',
-      },
-      {
-        color: '#613916',
-        label: 'espresso',
-      },
-    ],
-    image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
-  },
-];
-
-drinks.forEach((drink) => {
-  document.querySelector('.drinks-list').appendChild(Drink(drink));
-});
+fetch('https://apps.kodim.cz/daweb/cafelora/api/drinks')
+  .then((response) => response.json())
+  .then((data) => {
+    data.results.forEach((drink) => {
+      document.querySelector('.drinks-list').appendChild(Drink(drink));
+    });
+  });
